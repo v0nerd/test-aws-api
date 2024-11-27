b35c90aa77586071b7016e668d4844d884a14854
mltrev23 <trevor.dev23@gmail.com>
2024-11-27 13:32:19 -0600

    fix(service): add service apis

--- src/app.py
+++ src/app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from utils.run_doc import get_value 
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World from Flask and AppRunner!"
+
+@app.route('/test')
+def test():
+    return get_value("test")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
