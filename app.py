2a6557dfa53409f7ae5dbc8e9ec6addd444bed0b
mltrev23 <trevor.dev23@gmail.com>
2024-11-27 18:23:02 -0600

    all_started

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask
-from utils.run_doc import get_value 
+from fastapi import FastAPI
+from utils.run_doc import get_value
+from fastapi.responses import PlainTextResponse
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/')
-def hello():
-    return "Hello World from Flask and AppRunner!"
+@app.get("/")
+async def hello():
+    return "Hello World from FastAPI and AppRunner!"
 
-@app.route('/test')
-def test():
+@app.get("/test")
+async def test():
     return get_value("test")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8001)
+# To run the app, use the command: uvicorn app:app --host 0.0.0.0 --port 8001
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8001)
